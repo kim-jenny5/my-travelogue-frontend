04d3fbaf06aa6700231f56161101b058cf5b4b1c
Jenny Kim <kimj1905@gmail.com>
2021-12-01 14:25:22 -0500

    connected app with html

--- src/App.js
+++ src/App.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 export default class App extends Component {
 	render() {
-		return <div></div>;
+		return (
+			<div className="App">
+				<p>This is working now!</p>
+			</div>
+		);
 	}
 }
