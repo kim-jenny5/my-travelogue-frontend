1f4e1af99e316a45d9a3b1eaf9d798d553a77de4
Jenny Kim <kimj1905@gmail.com>
2021-12-01 16:12:08 -0500

    laid down the foundation for redux and thunk and react-router

--- src/index.js
+++ src/index.js
@@ -1,15 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-// import { Provider } from "react-redux";
-// import { createStore } from "redux";
-// import { rootReducer } from "./reducers/index";
+import { createStore, applyMiddleware } from "redux";
+import { Provider } from "react-redux";
+import rootReducer from "./reducers";
+import thunk from "redux-thunk";
+import { BrowserRouter as Router } from "react-router-dom";
 
-// let store = createStore(rootReducer);
+let store = createStore(rootReducer, applyMiddleware(thunk));
 
 ReactDOM.render(
-	// <Provider store={store}>
-	<App />,
-	// </Provider>,
+	<Provider store={store}>
+		<Router>
+			<App />
+		</Router>
+	</Provider>,
 	document.getElementById("root")
 );
