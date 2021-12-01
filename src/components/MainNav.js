d26bbd2047fb150bce5a469ac0f6ac15d079cf4e
Jenny Kim <kimj1905@gmail.com>
2021-12-01 17:24:16 -0500

    in the process of separating out different navbars for different pages

---
+++ src/components/MainNav.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { NavLink } from "react-router-dom";
+
+export const MainNav = () => {
+	return (
+		<div id="nav1">
+			<p>my travelogue</p>
+			<ul>
+				<li>
+					<NavLink to="/login">Log In</NavLink>
+				</li>
+				<li>
+					<NavLink to="/signup">Sign Up</NavLink>
+				</li>
+			</ul>
+		</div>
+	);
+};
--- src/containers/HomeContainer.js
+++ src/containers/HomeContainer.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import { MainNav } from "../components/MainNav";
 
 export default class HomeContainer extends Component {
 	render() {
 		return (
 			<div>
+				<MainNav />
 				<h1>
 					What's on my bucket list? <br /> <i>Everywhere.</i>
 				</h1>
