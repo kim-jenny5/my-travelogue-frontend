52609a84903a1ab293a1c4d80baf3a491042a934
Jenny Kim <kimj1905@gmail.com>
2021-12-01 19:05:31 -0500

    finished foundational coding for sign up form

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import { SecondNav } from "./SecondNav";
+import { NavLink } from "react-router-dom";
 
 export default class Signup extends Component {
 	render() {
@@ -7,6 +8,16 @@ export default class Signup extends Component {
 			<div>
 				<SecondNav />
 				<h1>Sign Up</h1>
+				<form>
+					<input type="text" placeholder="First Name" />
+					<input type="text" placeholder="Last Name" />
+					<input type="text" placeholder="Email Address" />
+					<input type="password" placeholder="Password" />
+					<button>Sign Up</button>
+				</form>
+				<p>
+					Already have an account? <NavLink to="/login">Log in</NavLink>
+				</p>
 			</div>
 		);
 	}
