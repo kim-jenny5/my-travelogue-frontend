5a034336645cae75d27974cf7e956a0a3f8c42b3
Jenny Kim <kimj1905@gmail.com>
2021-12-01 19:50:02 -0500

    added event handlers to login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,15 +3,44 @@ import { SecondNav } from "./SecondNav";
 import { NavLink } from "react-router-dom";
 
 export default class Login extends Component {
+	state = {
+		email: "",
+		password: ""
+	};
+
+	handleChange = (e) => {
+		this.setState(
+			{
+				[e.target.name]: e.target.value
+			},
+			() => console.log(this.state)
+		);
+	};
+
+	handleSubmit = (e) => {
+		e.preventDefault();
+		console.log(this.state);
+	};
+
 	render() {
 		// console.log(this.props);
 		return (
 			<div>
 				<SecondNav />
 				<h1>Log In</h1>
-				<form>
-					<input type="text" placeholder="Email Address" />
-					<input type="password" placeholder="Password" />
+				<form onSubmit={this.handleSubmit}>
+					<input
+						type="text"
+						placeholder="Email Address"
+						name="email"
+						onChange={this.handleChange}
+					/>
+					<input
+						type="password"
+						placeholder="Password"
+						name="password"
+						onChange={this.handleChange}
+					/>
 					<button type="submit">Log In</button>
 				</form>
 				<p>
