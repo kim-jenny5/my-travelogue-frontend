7404894bdbe7331a7e99bde09a5008bbeb7b1afc
Jenny Kim <kimj1905@gmail.com>
2021-12-01 19:02:39 -0500

    finished foundation coding for login form

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react";
 import { SecondNav } from "./SecondNav";
+import { NavLink } from "react-router-dom";
 
 export default class Login extends Component {
 	render() {
-		console.log(this.props);
+		// console.log(this.props);
 		return (
 			<div>
 				<SecondNav />
 				<h1>Log In</h1>
+				<form>
+					<input type="text" placeholder="Email Address" />
+					<input type="password" placeholder="Password" />
+					<button>Log In</button>
+				</form>
+				<p>
+					Don't have an account? <NavLink to="/signup">Sign Up</NavLink>
+				</p>
 			</div>
 		);
 	}
--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
+import { SecondNav } from "./SecondNav";
 
 export default class Signup extends Component {
 	render() {
 		return (
 			<div>
+				<SecondNav />
 				<h1>Sign Up</h1>
 			</div>
 		);
