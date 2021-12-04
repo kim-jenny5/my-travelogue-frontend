769996a148c229cc04b19d97dc51854011ec6ca5
Jenny Kim <kimj1905@gmail.com>
2021-12-04 16:17:53 -0500

    successfully set state for trips, but will need to consider user-trips has-many relationship

--- src/reducers/tripReducer.js
+++ src/reducers/tripReducer.js
@@ -4,13 +4,13 @@ const tripReducer = (
 ) => {
 	switch (action.type) {
 		case "CREATING_TRIP":
-			console.log(state);
-			console.log(action);
-			debugger;
+			// console.log(state);
+			// console.log(action);
+			// debugger;
 			return {
 				...state,
 				user: action.trip_info.user_id,
-				trips: [...action.trip_info],
+				trips: [...state.trips, action.trip_info],
 				creatingTrip: true,
 				createdTrip: false
 			};
