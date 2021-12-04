498851f737a5586be49ff9a22a385f7974d40cf9
Jenny Kim <kimj1905@gmail.com>
2021-12-04 13:43:40 -0500

    save before creating trip reducers and dispatches

--- src/components/NewTripForm.js
+++ src/components/NewTripForm.js
@@ -1,11 +1,26 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
 // import DateRangePicker from "react-daterange-picker";
 
-export default class NewTripForm extends Component {
+class NewTripForm extends Component {
 	state = {
-		name: "",
+		trip_name: "",
 		start_date: "",
 		end_date: ""
+	};
+
+	handleChange = (e) => {
+		// this.setState(
+		// 	{
+		// 		[e.target.name]: e.target.value
+		// 	},
+		// 	() => {
+		// 		console.log(this.state);
+		// 	}
+		// );
+		this.setState({
+			[e.target.name]: e.target.value
+		});
 	};
 
 	handleSubmit = (e) => {
@@ -20,14 +35,36 @@ export default class NewTripForm extends Component {
 					<div>Name of your trip</div>
 					<input
 						type="text"
+						name="trip_name"
 						placeholder="Example: Roadtrip with the girls 👯"
+						onChange={this.handleChange}
 					/>
 					{/* <DateRangePicker /> */}
-					<input type="date" id="start_date" name="start_date" />
-					<input type="date" id="end_date" name="end_date" />
+					<input
+						type="date"
+						id="start_date"
+						name="start_date"
+						onChange={this.handleChange}
+					/>
+					<input
+						type="date"
+						id="end_date"
+						name="end_date"
+						onChange={this.handleChange}
+					/>
 					<button type="submit">Let's Go!</button>
 				</form>
 			</div>
 		);
 	}
 }
+
+// const mapStateToProps = (state) => ({
+
+// })
+
+const mapDispatchToProps = (dispatch) => {
+    createTrip: state.
+};
+
+export default connect(null, mapDispatchToProps)(NewTripForm);
