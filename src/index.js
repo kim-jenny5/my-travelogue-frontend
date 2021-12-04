c7f4e9155398391fdfe8332d77292ac66a9b27fc
Jenny Kim <kimj1905@gmail.com>
2021-12-04 15:28:05 -0500

    fixed combinereducer not working--had to delete node_modules and re-yarn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
-import { createStore, applyMiddleware } from "redux";
+import { createStore, applyMiddleware, compose } from "redux";
 import { Provider } from "react-redux";
 import rootReducer from "./reducers/rootReducer";
 // import userReducer from "./reducers/userReducer";
