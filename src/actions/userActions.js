0ccbbcf227502f76500a2dfeb6c9bfe73f7a643f
Jenny Kim <kimj1905@gmail.com>
2021-12-03 23:31:02 -0500

    uncommented out fetching current user method for user's who are logged in

--- src/actions/userActions.js
+++ src/actions/userActions.js
@@ -1,24 +1,24 @@
 // import { Redirect } from "react-router-dom";
 
 export const fetchCurrentUser = () => {
-	return (dispatch) => {
-		// let token = localStorage.token;
-		// if (token) {
-		// 	return fetch("http://localhost:3000/authorized", {
-		// 		headers: {
-		// 			"Content-Type": "application/json",
-		// 			Accept: "application/json",
-		// 			Authorization: localStorage.token
-		// 		}
-		// 	})
-		// 		.then((resp) => resp.json())
-		// 		.then((data) => {
-		// 			console.log(data);
-		// 			if (data.error) alert(data.error);
-		// 			// dispatch();
-		// 			// debugger;
-		// 		});
-		// }
+	return () => {
+		let token = localStorage.token;
+		if (token) {
+			return fetch("http://localhost:3000/authorized", {
+				headers: {
+					"Content-Type": "application/json",
+					Accept: "application/json",
+					Authorization: localStorage.token
+				}
+			})
+				.then((resp) => resp.json())
+				.then((data) => {
+					console.log(data);
+					if (data.error) alert(data.error);
+					// dispatch();
+					// debugger;
+				});
+		}
 	};
 };
 
