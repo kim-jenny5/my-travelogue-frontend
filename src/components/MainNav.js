19fc44c3ff2fe3be6f5f410c29ba1e96696895bb
Jenny Kim <kimj1905@gmail.com>
2021-12-06 14:28:20 -0500

    scrapped styled components

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -2,30 +2,12 @@ import React from "react";
 import { NavLink } from "react-router-dom";
 import styled from "styled-components";
 
-const UserBtns = styled.button`
-	width: 53px;
-	font-size: 18px;
-	color: black;
-	background-color: ${(props) =>
-		props.type === "login" ? "transparent" : "#E0E0E0"};
-`;
-
 export const MainNav = () => {
 	return (
 		<div id="main-nav">
 			<p>my travelogue</p>
-			{/* <ul>
-				<li> */}
-			<UserBtns type="login">
-				<NavLink to="/login">Log In</NavLink>
-			</UserBtns>
-			{/* </li>
-				<li> */}
-			<UserBtns type="signup">
-				<NavLink to="/signup">Sign Up</NavLink>
-			</UserBtns>
-			{/* </li>
-			</ul> */}
+			<NavLink to="/login">Log In</NavLink>
+			<NavLink to="/signup">Sign Up</NavLink>
 		</div>
 	);
 };
