823e50f3d041d6877fbc5822c506edd07c2242f2
Jenny Kim <kimj1905@gmail.com>
2021-12-06 15:44:04 -0500

    somewhat designed footer on homepage

--- src/components/MainFooter.js
+++ src/components/MainFooter.js
@@ -1,12 +1,21 @@
 import React from "react";
+import { Envelope, LinkedInV1Fill, GithubFill } from "akar-icons";
 
 export const MainFooter = () => {
 	return (
-		<div>
-			<span>Email </span>
-			<span>LinkedIn </span>
-			<span>GitHub </span>
-			<span>© Jenny Kim, 2021</span>
+		<div className="footer main">
+			<span className="container">
+				<span className="icon-css">
+					<Envelope strokeWidth={2} size={20} className="icon-padding" />
+				</span>
+				<span className="icon-css">
+					<LinkedInV1Fill strokeWidth={2} size={20} className="icon-padding" />
+				</span>
+				<span className="icon-css">
+					<GithubFill strokeWidth={2} size={20} className="icon-padding" />
+				</span>
+				<span>© Jenny Kim, 2021</span>
+			</span>
 		</div>
 	);
 };
