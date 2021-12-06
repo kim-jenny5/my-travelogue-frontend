25e5c875d17f1a2ecd75f4906724eb8a2ad529a5
Jenny Kim <kimj1905@gmail.com>
2021-12-06 14:08:56 -0500

    added background color to user buttons on homepage

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,18 +1,31 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
+import styled from "styled-components";
+
+const UserBtns = styled.button`
+	width: 53px;
+	font-size: 18px;
+	color: black;
+	background-color: ${(props) =>
+		props.type === "login" ? "transparent" : "#E0E0E0"};
+`;
 
 export const MainNav = () => {
 	return (
 		<div id="main-nav">
 			<p>my travelogue</p>
-			<ul>
-				<li>
-					<NavLink to="/login">Log In</NavLink>
-				</li>
-				<li>
-					<NavLink to="/signup">Sign Up</NavLink>
-				</li>
-			</ul>
+			{/* <ul>
+				<li> */}
+			<UserBtns type="login">
+				<NavLink to="/login">Log In</NavLink>
+			</UserBtns>
+			{/* </li>
+				<li> */}
+			<UserBtns type="signup">
+				<NavLink to="/signup">Sign Up</NavLink>
+			</UserBtns>
+			{/* </li>
+			</ul> */}
 		</div>
 	);
 };
