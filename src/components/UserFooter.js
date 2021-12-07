2a238e0ea4f57798102cff41aac3fe7f41a7df91
Jenny Kim <kimj1905@gmail.com>
2021-12-07 14:51:27 -0500

    started on dashboard css

--- src/components/UserFooter.js
+++ src/components/UserFooter.js
@@ -3,8 +3,10 @@ import React from "react";
 export const UserFooter = (props) => {
 	const name = props.user.first_name.toLowerCase();
 	return (
-		<div>
-			<span>{name}'s travelogue </span>
+		<div className="footer user">
+			<div className="container">
+				<span>{name}'s travelogue </span>
+			</div>
 		</div>
 	);
 };
