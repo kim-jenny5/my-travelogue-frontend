e6199577cb9731fe08f38f64be23bced5e26f622
Jenny Kim <kimj1905@gmail.com>
2021-12-08 18:54:28 -0500

    edited user's footer

--- src/components/UserFooter.js
+++ src/components/UserFooter.js
@@ -1,12 +1,13 @@
 import React from "react";
 
 export const UserFooter = (props) => {
-	// debugger;
 	const name = props.first_name.toLowerCase();
 	return (
 		<div className="footer user">
 			<div className="container">
-				<span>{name}'s travelogue </span>
+				<span className="name">
+					{name}'s <i>travelogue</i>
+				</span>
 			</div>
 		</div>
 	);
