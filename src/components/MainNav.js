48312b351fb68a1bfd39df7e5c0d73fbf4c1f0e0
Jenny Kim <kimj1905@gmail.com>
2021-12-09 11:18:49 -0500

    added modal dependency and erased react-bootstrap

--- src/components/MainNav.js
+++ src/components/MainNav.js
@@ -1,19 +1,12 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
 
 export const MainNav = () => {
 	return (
 		<div className="nav main-nav">
-			{/* <Container>
-				<Row> */}
-			{/* <Col md={4}> */}
 			<span className="main-title homepage">
 				my <i>travelogue</i>
 			</span>
-			{/* </Col> */}
-			{/* <span> */}
 			<span className="container">
 				<NavLink to="/login" className="link user-btn login-btn">
 					Log In
@@ -22,8 +15,6 @@ export const MainNav = () => {
 					Sign Up
 				</NavLink>
 			</span>
-			{/* </Row>
-			</Container> */}
 		</div>
 	);
 };
