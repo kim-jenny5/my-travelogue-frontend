2ed6cdaab545dc39725b36d8664fcec1ba26e481
Jenny Kim <kimj1905@gmail.com>
2021-12-09 11:23:26 -0500

    created modal actions file

---
+++ src/actions/modalActions.js
@@ -0,0 +1,11 @@
+// export const showModal = () => {
+// 	return (dispatch) => {
+// 		dispatch({ type: "SHOW_MODAL" });
+// 	};
+// };
+
+// export const hideModal = () => {
+// 	return (dispatch) => {
+// 		dispatch({ type: "SHOW_MODAL" });
+// 	};
+// };
