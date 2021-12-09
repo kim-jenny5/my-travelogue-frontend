83201184080cad4069e791f8a5befb3f6dff2d14
Jenny Kim <kimj1905@gmail.com>
2021-12-08 20:35:36 -0500

    fixed destructuring error with upcoming trip information

--- src/components/NextTrip.js
+++ src/components/NextTrip.js
@@ -4,15 +4,18 @@ import DateFormatting from "./DateFormatting";
 export default function NextTrip(props) {
 	// console.log(props.trip);
 	// debugger;
-	const { days_left_till, trip_name, start_date, end_date } = props.trip;
-	// console.log(days_left_till, trip_name);
-	return (
-		<div>
-			<div>Upcoming Trip 🚗</div>
-			<div>{days_left_till}</div>
-			<div>{trip_name}</div>
-			<div>{DateFormatting(start_date, end_date)}</div>
-			{/* HELLO WOOHOO! */}
-		</div>
-	);
+	if (props.trip) {
+		const { days_left_till, trip_name, start_date, end_date } = props.trip;
+		return (
+			<div>
+				<div>Upcoming Trip 🚗</div>
+				<div>{days_left_till}</div>
+				<div>{trip_name}</div>
+				<div>{DateFormatting(start_date, end_date)}</div>
+				{/* HELLO WOOHOO! */}
+			</div>
+		);
+	} else {
+		return null;
+	}
 }
