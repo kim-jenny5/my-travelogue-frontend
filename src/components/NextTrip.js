8f5f0e933155b1d8e1274617fc2a56b2984954b6
Jenny Kim <kimj1905@gmail.com>
2021-12-08 20:27:07 -0500

    added upcoming trip information to its component

--- src/components/NextTrip.js
+++ src/components/NextTrip.js
@@ -1,5 +1,18 @@
 import React from "react";
+import DateFormatting from "./DateFormatting";
 
-export default function NextTrip() {
-	return <div>HELLO WOOHOO!</div>;
+export default function NextTrip(props) {
+	// console.log(props.trip);
+	// debugger;
+	const { days_left_till, trip_name, start_date, end_date } = props.trip;
+	// console.log(days_left_till, trip_name);
+	return (
+		<div>
+			<div>Upcoming Trip 🚗</div>
+			<div>{days_left_till}</div>
+			<div>{trip_name}</div>
+			<div>{DateFormatting(start_date, end_date)}</div>
+			{/* HELLO WOOHOO! */}
+		</div>
+	);
 }
