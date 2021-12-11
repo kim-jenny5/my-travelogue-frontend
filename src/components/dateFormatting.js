fdc468f64c8cd4484a999b34e540cb9bd1f50e5b
Jenny Kim <kimj1905@gmail.com>
2021-12-11 16:34:04 -0500

    changed naming convention for parameters to fit JS

--- src/components/dateFormatting.js
+++ src/components/dateFormatting.js
@@ -1,10 +1,10 @@
 import { isSameMonth, isSameYear, format } from "date-fns";
 
-export default function DateFormatting(start_date, end_date) {
-	// const start_date = new Date(trip.start_date);
-	// const end_date = new Date(trip.end_date);
-	const start = new Date(start_date);
-	const end = new Date(end_date);
+export default function DateFormatting(startDate, endDate) {
+	// const startDate = new Date(trip.startDate);
+	// const endDate = new Date(trip.endDate);
+	const start = new Date(startDate);
+	const end = new Date(endDate);
 	if (isSameMonth(start, end) && isSameYear(start, end)) {
 		return `${format(start, "MMMM d")} - ${format(end, "d, yyyy")}`;
 	} else if (isSameYear(start, end)) {
