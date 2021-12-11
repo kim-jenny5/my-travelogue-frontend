d3b3ae11b3d7915a80b0224b1ac7e10502d3d28f
Jenny Kim <kimj1905@gmail.com>
2021-12-11 13:59:35 -0500

    added an bug error message modal in case

--- src/components/Modals.js
+++ src/components/Modals.js
@@ -1,16 +1,30 @@
 import React from "react";
 import NewTripFormCont from "./NewTripFormCont";
+import { ChatError, Bug } from "akar-icons";
 
 export default function Modals(props) {
 	if (props.type === "new trip") {
 		return <NewTripFormCont />;
-		// } else if (props.type === "show trip") {
-		// 	return <div>SHOW TRIP!</div>;
+	} else if (props.type === "show trip") {
+		return <div>SHOW TRIP!</div>;
 	} else {
 		return ErrorModal();
 	}
 }
 
 const ErrorModal = () => {
-	return <div>Error</div>;
+	return (
+		<div className="error-container">
+			Oops... This is embarassing.{" "}
+			<span style={{ fontSize: "1.5em", verticalAlign: "middle" }}>😳</span>
+			<div
+				style={{ fontSize: "1.25rem", fontWeight: "400", marginBottom: "2em" }}
+			>
+				We're always bug hunting, but we'll be sure to catch this one too!
+			</div>
+			<div>
+				<Bug strokeWidth={2} size={60} />
+			</div>
+		</div>
+	);
 };
