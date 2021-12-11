21a02ca468cf354e61e53bca08770346b0e1a174
Jenny Kim <kimj1905@gmail.com>
2021-12-11 13:01:58 -0500

    added links to icons on the footer

--- src/components/MainFooter.js
+++ src/components/MainFooter.js
@@ -5,9 +5,15 @@ export const MainFooter = () => {
 	return (
 		<div className="footer main">
 			<div className="container">
-				<Envelope strokeWidth={2} size={18} className="icon-css" />
-				<LinkedInV1Fill strokeWidth={2} size={18} className="icon-css" />
-				<GithubFill strokeWidth={2} size={18} className="icon-css" />
+				<a href="mailto:jennykimdev@gmail.com" target="_blank">
+					<Envelope strokeWidth={2} size={18} className="icon-css" />
+				</a>
+				<a href="https://www.linkedin.com/in/kim-jenny5/" target="_blank">
+					<LinkedInV1Fill strokeWidth={2} size={18} className="icon-css" />
+				</a>
+				<a href="https://github.com/kim-jenny5" target="_blank">
+					<GithubFill strokeWidth={2} size={18} className="icon-css" />
+				</a>
 				<span className="copyright">© Jenny Kim, 2021</span>
 			</div>
 		</div>
