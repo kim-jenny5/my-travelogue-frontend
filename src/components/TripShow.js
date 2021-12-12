c6b14a8375a7c7d95f3a30266aa69942f11074e2
Jenny Kim <kimj1905@gmail.com>
2021-12-11 21:50:46 -0500

    started adding form to add places to trip itinerary

--- src/components/TripShow.js
+++ src/components/TripShow.js
@@ -1,5 +1,6 @@
 import React from "react";
 import DateFormatting from "./DateFormatting";
+// import TextField from "@mui/material/TextField";
 
 export default function TripShow(props) {
 	const { tripName, startDate, endDate } = props.trip;
@@ -8,6 +9,9 @@ export default function TripShow(props) {
 		<div>
 			<div className="show-page title">{tripName}</div>
 			<div>{DateFormatting(startDate, endDate)}</div>
+			{/* <form onSubmit={}>
+				<TextField id="standard-basic" label="Standard" variant="standard" />
+			</form> */}
 		</div>
 	);
 }
--- src/components/TripShowCont.js
+++ src/components/TripShowCont.js
@@ -1,16 +1,38 @@
 import React, { Component } from "react";
 import { connect } from "react-redux";
-import { fetchATrip } from "../actions/tripActions";
+// import { fetchATrip } from "../actions/tripActions";
 import TripShow from "./TripShow";
+import TextField from "@mui/material/TextField";
+import IconButton from "@mui/material/IconButton";
+import { Plus, CirclePlus } from "akar-icons";
 
 class TripShowCont extends Component {
+	handleSubmit = (e) => {
+		e.preventDefault();
+		console.log("yolo");
+	};
+
 	render() {
 		const notNull = Object.values(this.props.fetchedTrip).every(
 			(value) => value !== null
 		);
 
 		if (notNull) {
-			return <TripShow trip={this.props.fetchedTrip} />;
+			return (
+				<div>
+					<TripShow trip={this.props.fetchedTrip} />
+					<form onSubmit={this.handleSubmit}>
+						<TextField
+							id="standard-basic"
+							label="Standard"
+							variant="standard"
+						/>
+						<IconButton color="primary" type="submit">
+							<CirclePlus strokeWidth={2} size={20} />
+						</IconButton>
+					</form>
+				</div>
+			);
 		} else return null;
 	}
 }
